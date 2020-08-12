9c93e44116c46040cd07fcb2530033cf2566f677
Putu Eka Mulyana <putuekamulyana97@gmail.com>
2020-08-12 12:01:00 +0800

    categori doctor screen

--- src/pages/Doctor/index.js
+++ src/pages/Doctor/index.js
@@ -5,24 +5,39 @@ import {
   DoctorCategory,
   RatedDoctor,
   NewItem,
+  Gap,
 } from '../../components';
 import {fonts, colors} from '../../utils';
+import {ScrollView} from 'react-native-gesture-handler';
 const Doctor = () => {
   return (
     <View style={styles.page}>
-      <HomeProfile />
-      <Text style={styles.welcome}>Mau konsultasi dengan siapa hari ini?</Text>
-      <DoctorCategory />
-      <DoctorCategory />
-      <DoctorCategory />
-      <Text>Top Rated Doctors</Text>
-      <RatedDoctor />
-      <RatedDoctor />
-      <RatedDoctor />
-      <Text>Good News</Text>
-      <NewItem />
-      <NewItem />
-      <NewItem />
+      <View style={styles.content}>
+        <HomeProfile />
+        <Text style={styles.welcome}>
+          Mau konsultasi dengan siapa hari ini?
+        </Text>
+        <View style={styles.wrepperScroll}>
+          <ScrollView horizontal showsHorizontalScrollIndicator={false}>
+            <View style={styles.categorys}>
+              <Gap width={16} />
+              <DoctorCategory />
+              <DoctorCategory />
+              <DoctorCategory />
+              <DoctorCategory />
+              <Gap width={6} />
+            </View>
+          </ScrollView>
+        </View>
+        <Text style={styles.sectionLabel}>Top Rated Doctors</Text>
+        <RatedDoctor />
+        <RatedDoctor />
+        <RatedDoctor />
+        <Text style={styles.sectionLabel}>Good News</Text>
+        <NewItem />
+        <NewItem />
+        <NewItem />
+      </View>
     </View>
   );
 };
@@ -31,11 +46,32 @@ export default Doctor;
 
 const styles = StyleSheet.create({
   page: {
-    marginVertical: 34,
+    backgroundColor: colors.secondary,
+    flex: 1,
+  },
+  content: {
+    backgroundColor: colors.white,
+    flex: 1,
+    paddingVertical: 34,
     paddingHorizontal: 16,
+    borderBottomLeftRadius: 20,
+    borderBottomRightRadius: 20,
   },
   welcome: {
     fontSize: 20,
+    fontFamily: fonts.primary[600],
+    color: colors.text.primary,
+    marginTop: 30,
+    marginBottom: 16,
+  },
+  categorys: {
+    flexDirection: 'row',
+  },
+  wrepperScroll: {
+    marginHorizontal: -16,
+  },
+  sectionLabel: {
+    fontSize: 16,
     fontFamily: fonts.primary[600],
     color: colors.text.primary,
     marginTop: 30,
--- src/utils/colors/index.js
+++ src/utils/colors/index.js
@@ -1,5 +1,6 @@
 const mainColors = {
   green1: '#0BCAD4',
+  green2: '#EDFCFD',
   dark1: '#112340',
   dark2: '#495A75',
   gray1: '#7D8797',
@@ -27,4 +28,5 @@ export const colors = {
     },
   },
   border: mainColors.gray2,
+  cardLigth: mainColors.gray2,
 };
