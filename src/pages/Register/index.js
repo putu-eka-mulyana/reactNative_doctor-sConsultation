3703a13399425efb778806426ab6b904e3e1fedc
Putu Eka Mulyana <putuekamulyana97@gmail.com>
2020-09-24 00:43:36 +0800

    setup firebase

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,25 +1,50 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {ScrollView, StyleSheet, View} from 'react-native';
 import {Button, Gap, Header, Input} from '../../components';
-import {colors} from '../../utils';
+import {colors, useForm} from '../../utils';
 
 const Register = ({navigation}) => {
+  const [form, setForm] = useForm({
+    fullName: '',
+    profession: '',
+    email: '',
+    password: '',
+  });
   return (
     <View style={styles.page}>
       <Header title="Daftar Akun" onPress={() => navigation.goBack()} />
       <View style={styles.container}>
-        <Input label="Full Name" />
-        <Gap height={24} />
-        <Input label="Profession" />
-        <Gap height={24} />
-        <Input label="Email Address" />
-        <Gap height={24} />
-        <Input label="Password" />
-        <Gap height={40} />
-        <Button
-          title="Continue"
-          onPress={() => navigation.navigate('UploadPhoto')}
-        />
+        <ScrollView showsVerticalScrollIndicator={false}>
+          <Input
+            label="Full Name"
+            value={form.fullName}
+            onChangeText={(value) => setForm('fullName', value)}
+          />
+          <Gap height={24} />
+          <Input
+            label="Profession"
+            value={form.profession}
+            onChangeText={(value) => setForm('profession', value)}
+          />
+          <Gap height={24} />
+          <Input
+            label="Email Address"
+            value={form.email}
+            onChangeText={(value) => setForm('email', value)}
+          />
+          <Gap height={24} />
+          <Input
+            label="Password"
+            value={form.password}
+            onChangeText={(value) => setForm('password', value)}
+            secureTextEntry
+          />
+          <Gap height={40} />
+          <Button
+            title="Continue"
+            onPress={() => navigation.navigate('UploadPhoto')}
+          />
+        </ScrollView>
       </View>
     </View>
   );
